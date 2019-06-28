903f674257d075d744f643edc8269132fa928d7e
Ryan Leckey <ryan@launchbadge.com>
2019-06-28 00:30:12 -0700

    Assume Postgres isn't lying (use UTF-8 unchecked)

--- sqlx-postgres-protocol/src/decode.rs
+++ sqlx-postgres-protocol/src/decode.rs
@@ -12,7 +12,7 @@ pub trait Decode {
 pub(crate) fn get_str(src: &[u8]) -> io::Result<&str> {
     let end = memchr(b'\0', &src).ok_or(io::ErrorKind::UnexpectedEof)?;
     let buf = &src[..end];
-    let s = str::from_utf8(buf).map_err(|_| io::ErrorKind::InvalidData)?;
+    let s = unsafe { str::from_utf8_unchecked(buf) };
 
     Ok(s)
 }
