3d01db84a0c08addac956b79d657b38e9c5f5899
Ryan Leckey <ryan@launchbadge.com>
2019-07-26 08:36:28 -0700

    Add create/drop database to the main driver

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,30 @@ use std::io;
 // TODO: ToSql and FromSql (to [de]serialize values from/to Rust and SQL)
 // TODO: Connection strings ala postgres@localhost/sqlx_dev
 
-#[runtime::main]
+#[runtime::main(runtime_tokio::Tokio)]
 async fn main() -> io::Result<()> {
-    env_logger::init();
+    // Connect as postgres / postgres and DROP the sqlx__dev database
+    // if exists and then re-create it
+    let mut conn = Connection::establish(
+        ConnectOptions::new()
+            .host("127.0.0.1")
+            .port(5432)
+            .user("postgres")
+            .database("postgres"),
+    )
+    .await?;
+
+    println!(" :: drop database (if exists) sqlx__dev");
+
+    conn.prepare("DROP DATABASE IF EXISTS sqlx__dev")
+        .execute()
+        .await?;
+
+    println!(" :: create database sqlx__dev");
+
+    conn.prepare("CREATE DATABASE sqlx__dev").execute().await?;
+
+    conn.close().await?;
 
     let mut conn = Connection::establish(
         ConnectOptions::new()
@@ -19,6 +40,8 @@ async fn main() -> io::Result<()> {
             .database("sqlx__dev"),
     )
     .await?;
+
+    println!(" :: create schema");
 
     conn.prepare(
         r#"
@@ -31,18 +54,26 @@ CREATE TABLE IF NOT EXISTS users (
     .execute()
     .await?;
 
-    let new_id = conn
+    println!(" :: insert");
+
+    let new_row = conn
         .prepare("INSERT INTO users (name) VALUES ($1) RETURNING id")
         .bind(b"Joe")
         .get()
         .await?;
 
+    let new_id = new_row.as_ref().unwrap().get(0);
+
     println!("insert {:?}", new_id);
+
+    println!(" :: select");
 
     conn.prepare("SELECT id FROM users")
         .select()
         .try_for_each(|row| {
-            println!("select {:?}", row.get(0));
+            let id = row.get(0);
+
+            println!("select {:?}", id);
 
             future::ok(())
         })
