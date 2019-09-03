451e0ee73fe13da2512976cf3ecf23015f3b6191
Ryan Leckey <ryan@launchbadge.com>
2019-09-03 06:54:51 -0700

    Parse outputs a statement, not a portal

--- src/postgres/protocol/parse.rs
+++ src/postgres/protocol/parse.rs
@@ -3,7 +3,7 @@ use crate::io::BufMut;
 use byteorder::NetworkEndian;
 
 pub struct Parse<'a> {
-    pub portal: &'a str,
+    pub statement: &'a str,
     pub query: &'a str,
     pub param_types: &'a [u32],
 }
@@ -12,11 +12,13 @@ impl Encode for Parse<'_> {
     fn encode(&self, buf: &mut Vec<u8>) {
         buf.push(b'P');
 
-        // len + portal + nul + query + null + len(param_types) + param_types
-        let len = 4 + self.portal.len() + 1 + self.query.len() + 1 + 2 + self.param_types.len() * 4;
+        // len + statement + nul + query + null + len(param_types) + param_types
+        let len =
+            4 + self.statement.len() + 1 + self.query.len() + 1 + 2 + self.param_types.len() * 4;
+
         buf.put_i32::<NetworkEndian>(len as i32);
 
-        buf.put_str_nul(self.portal);
+        buf.put_str_nul(self.statement);
         buf.put_str_nul(self.query);
 
         buf.put_i16::<NetworkEndian>(self.param_types.len() as i16);
