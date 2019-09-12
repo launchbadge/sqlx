5b6c4ad8fac03bd3e159b7b7fcee7b1cb1438ff7
Ryan Leckey <ryan@launchbadge.com>
2019-09-11 22:25:41 -0700

    Re-arrange tests for postgres connection

--- src/postgres/connection.rs
+++ src/postgres/connection.rs
@@ -93,3 +93,81 @@ impl RawConnection for PostgresRawConnection {
         Ok(row)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::query::QueryParameters;
+    use std::env;
+
+    fn database_url() -> String {
+        env::var("POSTGRES_DATABASE_URL")
+            .or_else(|_| env::var("DATABASE_URL"))
+            .unwrap()
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn it_establishes() -> crate::Result<()> {
+        let mut conn = PostgresRawConnection::establish(&database_url()).await?;
+
+        // After establish, run PING to ensure that it was established correctly
+        conn.ping().await?;
+
+        // Then explicitly close the connection
+        conn.close().await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn it_executes() -> crate::Result<()> {
+        let mut conn = PostgresRawConnection::establish(&database_url()).await?;
+
+        let affected_rows_from_begin =
+            RawConnection::execute(&mut conn, "BEGIN", PostgresQueryParameters::new()).await?;
+
+        assert_eq!(affected_rows_from_begin, 0);
+
+        let affected_rows_from_create_table = RawConnection::execute(
+            &mut conn,
+            r#"
+CREATE TEMP TABLE sqlx_test_it_executes (
+    id BIGSERIAL PRIMARY KEY
+)
+                "#,
+            PostgresQueryParameters::new(),
+        )
+        .await?;
+
+        assert_eq!(affected_rows_from_create_table, 0);
+
+        for _ in 0..5_i32 {
+            let affected_rows_from_insert = RawConnection::execute(
+                &mut conn,
+                "INSERT INTO sqlx_test_it_executes DEFAULT VALUES",
+                PostgresQueryParameters::new(),
+            )
+            .await?;
+
+            assert_eq!(affected_rows_from_insert, 1);
+        }
+
+        let affected_rows_from_delete = RawConnection::execute(
+            &mut conn,
+            "DELETE FROM sqlx_test_it_executes",
+            PostgresQueryParameters::new(),
+        )
+        .await?;
+
+        assert_eq!(affected_rows_from_delete, 5);
+
+        let affected_rows_from_rollback =
+            RawConnection::execute(&mut conn, "ROLLBACK", PostgresQueryParameters::new()).await?;
+
+        assert_eq!(affected_rows_from_rollback, 0);
+
+        Ok(())
+    }
+}
