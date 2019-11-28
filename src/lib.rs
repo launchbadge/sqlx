d307e3cccfb7a4b453db7e5d861c562c02bdbaca
Ryan Leckey <ryan@launchbadge.com>
2019-11-27 22:35:34 -0800

    Collapse a cfg attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-#[cfg(feature = "macros")]
-use proc_macro_hack::proc_macro_hack;
-
 #[doc(inline)]
 pub use sqlx_core::*;
 
 #[cfg(feature = "macros")]
-#[proc_macro_hack(fake_call_site)]
+#[proc_macro_hack::proc_macro_hack(fake_call_site)]
 pub use sqlx_macros::query;
