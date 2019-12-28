61237b8503c353049555eb2b0f3619269e4d087f
Ryan Leckey <ryan@launchbadge.com>
2019-12-27 22:47:39 -0800

    Implement Register and GetCurrentUser for RealWorld

--- examples/realworld-postgres/src/main.rs
+++ examples/realworld-postgres/src/main.rs
@@ -1,6 +1,10 @@
+use chrono::{Duration, Utc};
+use rand::{thread_rng, RngCore};
 use sqlx::PgPool;
 use std::env;
 use tide::{Request, Response};
+
+const SECRET_KEY: &str = "this-is-the-most-secret-key-ever-secreted";
 
 // NOTE: Tide 0.5.x does not handle errors so any fallible methods just [.unwrap] for the moment.
 //       To be clear, that is not recommended and this should be fixed as soon as Tide fixes its
@@ -8,17 +12,27 @@ use tide::{Request, Response};
 
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv::dotenv()?;
-
     let pool = PgPool::new(&env::var("DATABASE_URL")?).await?;
 
     let mut server = tide::with_state(pool);
 
     server.at("/api/users").post(register);
 
-    server.listen(("0.0.0.0", 8080)).await?;
+    server.at("/api/user").get(get_current_user);
+
+    server.listen(("localhost", 8080)).await?;
 
     Ok(())
+}
+
+// User
+// https://github.com/gothinkster/realworld/tree/master/api#users-for-authentication
+
+#[derive(serde::Serialize)]
+struct User {
+    email: String,
+    token: String,
+    username: String,
 }
 
 // Registration
@@ -34,28 +48,133 @@ async fn register(mut req: Request<PgPool>) -> Response {
     }
 
     let body: RegisterRequestBody = req.body_json().await.unwrap();
+    let hash = hash_password(&body.password).unwrap();
 
     let mut pool = req.state();
 
-    let (user_id,): (i64,) = sqlx::query!(
+    let rec = sqlx::query!(
         r#"
-INSERT INTO users ( username, email )
-VALUES ( $1, $2 )
-RETURNING id
+INSERT INTO users ( username, email, password )
+VALUES ( $1, $2, $3 )
+RETURNING id, username, email
             "#,
         &*body.username,
-        &*body.email
+        &*body.email,
+        &*hash,
+    )
+    .fetch_one(&mut pool)
+    .await
+    .unwrap();
+
+    let token = generate_token(rec.id).unwrap();
+
+    #[derive(serde::Serialize)]
+    struct RegisterResponseBody {
+        user: User,
+    }
+
+    Response::new(200)
+        .body_json(&RegisterResponseBody {
+            user: User {
+                username: rec.username,
+                email: rec.email,
+                token,
+            },
+        })
+        .unwrap()
+}
+
+// Get Current User
+// https://github.com/gothinkster/realworld/tree/master/api#get-current-user
+
+// #[get("/api/user")]
+async fn get_current_user(req: Request<PgPool>) -> Response {
+    // TODO: Combine these methods? &Request isn't Sync though
+    let token = get_token_from_request(&req);
+    let user_id = authorize(&token).await.unwrap();
+
+    let mut pool = req.state();
+
+    let rec = sqlx::query!(
+        r#"
+SELECT username, email
+FROM users
+WHERE id = $1
+        "#,
+        user_id,
     )
     .fetch_one(&mut pool)
     .await
     .unwrap();
 
     #[derive(serde::Serialize)]
-    struct RegisterResponseBody {
-        id: i64,
+    struct GetCurrentUserResponseBody {
+        user: User,
     }
 
     Response::new(200)
-        .body_json(&RegisterResponseBody { id: user_id })
+        .body_json(&GetCurrentUserResponseBody {
+            user: User {
+                username: rec.username,
+                email: rec.email,
+                token,
+            },
+        })
         .unwrap()
+}
+
+fn get_token_from_request(req: &Request<PgPool>) -> String {
+    req.header("authorization")
+        .unwrap_or_default()
+        .splitn(2, ' ')
+        .nth(1)
+        .unwrap_or_default()
+        .to_owned()
+}
+
+async fn authorize(token: &str) -> anyhow::Result<i64> {
+    let data = jsonwebtoken::decode::<TokenClaims>(
+        token,
+        SECRET_KEY.as_ref(),
+        &jsonwebtoken::Validation::default(),
+    )?;
+
+    Ok(data.claims.sub)
+}
+
+// TODO: Does this need to be spawned in async-std ?
+fn hash_password(password: &str) -> anyhow::Result<String> {
+    let salt = generate_random_salt();
+    let hash = argon2::hash_encoded(password.as_bytes(), &salt, &argon2::Config::default())?;
+
+    Ok(hash)
+}
+
+fn generate_random_salt() -> [u8; 16] {
+    let mut salt = [0; 16];
+    thread_rng().fill_bytes(&mut salt);
+
+    salt
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct TokenClaims {
+    sub: i64,
+    exp: i64,
+}
+
+fn generate_token(user_id: i64) -> anyhow::Result<String> {
+    use jsonwebtoken::Header;
+
+    let exp = Utc::now() + Duration::hours(1);
+    let token = jsonwebtoken::encode(
+        &Header::default(),
+        &TokenClaims {
+            sub: user_id,
+            exp: exp.timestamp(),
+        },
+        SECRET_KEY.as_ref(),
+    )?;
+
+    Ok(token)
 }
