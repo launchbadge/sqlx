fba075ec02874916d2ccf42c16801cd7b7e586b3
Ryan Leckey <ryan@launchbadge.com>
2019-12-30 02:09:53 -0800

    Turn on more tests in test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -e
 cargo test -p sqlx-core --all-features
 
 # Postgres
-env DATABASE_URL="postgres://" cargo test -p sqlx --no-default-features --features 'postgres uuid chrono'
+env DATABASE_URL="postgres://" cargo test -p sqlx --no-default-features --features 'postgres macros uuid chrono'
 
-# MySQL
-env DATABASE_URL="mysql:///sqlx" cargo test -p sqlx --no-default-features --features 'mysql'
+# MySQL (requires sqlx database)
+env DATABASE_URL="mysql:///sqlx" cargo test -p sqlx --no-default-features --features 'mysql chrono'
