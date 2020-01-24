1e9e816fab374c8e070defbac5d7e8c07d1b9db0
Tom Dohrmann <erbse.13@gmx.de>
2020-01-24 18:36:04 +0100

    add proper encode/decode test

--- tests/derives.rs
+++ tests/derives.rs
@@ -1,27 +1,60 @@
+use sqlx::decode::Decode;
+use sqlx::encode::Encode;
+
+#[derive(PartialEq, Debug, Encode, Decode)]
+struct Foo(i32);
+
 #[test]
-#[cfg(feature = "macros")]
-fn encode() {
-    use sqlx::encode::Encode;
-
-    #[derive(Encode)]
-    struct Foo(i32);
-
-    #[cfg(feature = "postgres")]
-    let _: Box<dyn Encode<sqlx::Postgres>> = Box::new(Foo(1));
-    #[cfg(feature = "mysql")]
-    let _: Box<dyn Encode<sqlx::MySql>> = Box::new(Foo(1));
+#[cfg(feature = "mysql")]
+fn encode_mysql() {
+    encode_with_db::<sqlx::MySql>();
 }
 
 #[test]
-#[cfg(feature = "macros")]
-fn decode() {
-    use sqlx::decode::Decode;
+#[cfg(feature = "postgres")]
+fn encode_postgres() {
+    encode_with_db::<sqlx::Postgres>();
+}
 
-    #[derive(Decode)]
-    struct Foo(i32);
+#[allow(dead_code)]
+fn encode_with_db<DB: sqlx::Database>()
+where
+    Foo: Encode<DB>,
+    i32: Encode<DB>,
+{
+    let example = Foo(0x1122_3344);
 
-    #[cfg(feature = "postgres")]
-    <Foo as Decode<sqlx::Postgres>>::decode_null().ok();
-    #[cfg(feature = "mysql")]
-    <Foo as Decode<sqlx::MySql>>::decode_null().ok();
+    let mut encoded = Vec::new();
+    let mut encoded_orig = Vec::new();
+
+    Encode::<DB>::encode(&example, &mut encoded);
+    Encode::<DB>::encode(&example.0, &mut encoded_orig);
+
+    assert_eq!(encoded, encoded_orig);
+}
+
+#[test]
+#[cfg(feature = "mysql")]
+fn decode_mysql() {
+    decode_with_db::<sqlx::MySql>();
+}
+
+#[test]
+#[cfg(feature = "postgres")]
+fn decode_postgres() {
+    decode_with_db::<sqlx::Postgres>();
+}
+
+#[allow(dead_code)]
+fn decode_with_db<DB: sqlx::Database>()
+where
+    Foo: Decode<DB> + Encode<DB>,
+{
+    let example = Foo(0x1122_3344);
+
+    let mut encoded = Vec::new();
+    Encode::<DB>::encode(&example, &mut encoded);
+
+    let decoded = Foo::decode(&encoded).unwrap();
+    assert_eq!(example, decoded);
 }
