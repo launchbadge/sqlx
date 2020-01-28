85e23e30b5f94e9258d6699b137d4e471b014865
Austin Bonander <austin@launchbadge.com>
2020-01-27 22:28:24 -0800

    WIP experimenting with a replacement for `TyCons[Ext]`

--- src/ty_cons.rs
+++ src/ty_cons.rs
@@ -5,95 +5,107 @@ use std::marker::PhantomData;
 
 // For query parameters, database gives us a single type ID which we convert to an "expected" or
 // preferred Rust type, but there can actually be several types that are compatible for a given type
-// in input position.
+// in input position. E.g. for an expected parameter of `String`, we want to accept `String`,
+// `Option<String>`, `&str` and `Option<&str>`. And for the best compiler errors we don't just
+// want an `IsCompatible` trait (at least not without `#[on_unimplemented]` which is unstable
+// for the foreseeable future).
 
 // We can do this by using autoref (for method calls, the compiler adds reference ops until
 // it finds a matching impl) with impls that technically don't overlap as a hacky form of
 // specialization (but this works only if all types are statically known, i.e. we're not in a
 // generic context; this should suit 99% of use cases for the macros).
 
-#[doc(hidden)]
-pub struct TyCons<T: ?Sized>(PhantomData<T>);
+pub struct InnerType<T>(PhantomData<T>);
 
-impl<T: ?Sized> TyCons<T> {
-    pub fn new(_t: &T) -> TyCons<T> {
-        TyCons(PhantomData)
+impl<T: Sized> InnerType<T> {
+    fn new(_t: T) -> Self { InnerType(PhantomData) }
+}
+
+pub trait InnerTypeExt: Sized {
+    type Inner: Sized;
+    fn inner_type(self) -> Self::Inner {
+        panic!("only for type resolution")
     }
 }
 
-#[doc(hidden)]
-pub trait TyConsExt: Sized {
-    type Cons;
-    fn ty_cons(self) -> Self::Cons {
-        panic!("should not be run, only for type resolution")
+impl<T> InnerTypeExt for InnerType<Option<T>> {
+    type Inner = T;
+}
+
+impl<T> InnerTypeExt for &'_ InnerType<T> {
+    type Inner = T;
+}
+
+pub struct MatchBorrow<T, U>(PhantomData<T>, PhantomData<U>);
+
+impl<T, U> MatchBorrow<T, U> {
+    fn new(_u: U) -> Self { MatchBorrow(PhantomData, PhantomData) }
+}
+
+pub trait MatchBorrowExt: Sized {
+    type Matched;
+
+    fn match_borrow(self) -> Self::Matched {
+        panic!("only for type resolution")
     }
-
-    // if we're two resolutions deep, e.g. trying to match `Option<&str>` with `String`,
-    // lift through one of them.
-    // https://github.com/launchbadge/sqlx/issues/93
-    fn lift(self) -> TyCons<Self::Cons> {
-        TyCons::new(&self.ty_cons())
-    }
 }
 
-impl<T> TyCons<Option<&'_ T>> {
-    pub fn ty_cons(self) -> T {
-        panic!("should not be run, only for type resolution")
-    }
+impl<'a> MatchBorrowExt for MatchBorrow<String, &'a str> {
+    type Matched = &'a str;
 }
 
-// no overlap with the following impls because of the `: Sized` bound
-impl<T: Sized> TyConsExt for TyCons<&'_ T> {
-    type Cons = T;
+impl<'a> MatchBorrowExt for MatchBorrow<Vec<u8>, &'a [u8]> {
+    type Matched = &'a [u8];
 }
 
-impl TyConsExt for TyCons<str> {
-    type Cons = String;
+impl<'a, T: 'a, U: 'a> MatchBorrowExt for MatchBorrow<T, &'a U> {
+    type Matched = &'a U;
 }
 
-impl<T> TyConsExt for TyCons<[T]> {
-    type Cons = Vec<T>;
+impl<T, U> MatchBorrowExt for &'_ MatchBorrow<T, U> {
+    type Matched = T;
 }
 
-impl TyConsExt for TyCons<&'_ str> {
-    type Cons = String;
-}
-
-impl<T> TyConsExt for TyCons<&'_ [T]> {
-    type Cons = Vec<T>;
-}
-
-impl<T> TyConsExt for TyCons<Option<T>> {
-    type Cons = T;
-}
-
-impl<T> TyConsExt for &'_ TyCons<T> {
-    type Cons = T;
+fn conjure_value<T>() -> T {
+    panic!()
 }
 
 #[test]
 fn test_tycons_ext() {
     if false {
-        let _: u64 = TyCons::new(&Some(5u64)).lift().ty_cons();
-        let _: u64 = TyCons::new(&Some(&5u64)).lift().ty_cons();
-        let _: u64 = TyCons::new(&&5u64).lift().ty_cons();
-        let _: u64 = TyCons::new(&5u64).lift().ty_cons();
+        let mut arg: u64 = InnerType::new(Some(5u64)).inner_type();
+        arg = MatchBorrow::<u64, _>::new(arg).match_borrow();
 
-        // Option<&str>
-        let _: String = TyCons::new(&Some("Hello, world!")).lift().ty_cons();
-        // Option<String>
-        let _: String = TyCons::new(&Some("Hello, world!".to_string()))
-            .lift()
-            .ty_cons();
-        // Option<&String>
-        let _: String = TyCons::new(&Some(&"Hello, world!".to_string()))
-            .lift()
-            .ty_cons();
-        // &str
-        let _: String = TyCons::new(&"Hello, world!").lift().ty_cons();
-        // String
-        let _: String = TyCons::new(&"Hello, world!".to_string()).lift().ty_cons();
-        // str
-        let _: String = TyCons::new("Hello, world!").lift().ty_cons();
+        let mut arg: &u64 = InnerType::new(Some(&5u64)).inner_type();
+        arg = MatchBorrow::<u64, _>::new(arg).match_borrow();
+
+        let mut arg: &u64 = InnerType::new(&5u64).inner_type();
+        arg = MatchBorrow::<u64, _>::new(arg).match_borrow();
+
+        let mut arg: &u64 = InnerType::new(&&5u64).inner_type();
+        arg = MatchBorrow::<u64, _>::new(arg).match_borrow();
+
+        let mut arg: &str = InnerType::new("Hello, world").inner_type();
+        arg = MatchBorrow::<String, _>::new(arg).match_borrow();
+
+        let mut arg: &&str = InnerType::new(&"Hello, world").inner_type();
+        arg = MatchBorrow::<String, _>::new(arg).match_borrow();
+
+        let mut arg: &str = InnerType::new(Some("Hello, world")).inner_type();
+        arg = MatchBorrow::<String, _>::new(arg).match_borrow();
+
+        let s = "Hello, world".to_string();
+        let mut arg: &String = InnerType::new(Some(&s)).inner_type();
+        arg = MatchBorrow::<String, _>::new(arg).match_borrow();
+
+        let mut arg: String = InnerType::new(Some(s)).inner_type();
+        arg = MatchBorrow::<String, _>::new(arg).match_borrow();
+
+        let s = "Hello, world".to_string();
+        let mut arg: &String = InnerType::new(&s).inner_type();
+        arg = MatchBorrow::<String, _>::new(arg).match_borrow();
+
+        let mut arg: String = InnerType::new(s).inner_type();
+        arg = MatchBorrow::<String, _>::new(arg).match_borrow();
     }
 }
