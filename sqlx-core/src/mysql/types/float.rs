2349978417b2077222c4d947f53a31f71261981c
Austin Bonander <austin@launchbadge.com>
2020-01-29 23:37:15 -0800

    fix and test floats in MySQL

--- sqlx-core/src/mysql/types/float.rs
+++ sqlx-core/src/mysql/types/float.rs
@@ -5,6 +5,19 @@ use crate::mysql::types::MySqlTypeInfo;
 use crate::mysql::MySql;
 use crate::types::HasSqlType;
 
+/// The equivalent MySQL type for `f32` is `FLOAT`.
+///
+/// ### Note
+/// While we added support for `f32` as `FLOAT` for completeness, we don't recommend using
+/// it for any real-life applications as it cannot precisely represent some fractional values,
+/// and may be implicitly widened to `DOUBLE` in some cases, resulting in a slightly different
+/// value:
+///
+/// ```rust
+/// // Widening changes the equivalent decimal value, these two expressions are not equal
+/// // (This is expected behavior for floating points and happens both in Rust and in MySQL)
+/// assert_ne!(10.2f32 as f64, 10.2f64);
+/// ```
 impl HasSqlType<f32> for MySql {
     fn type_info() -> MySqlTypeInfo {
         MySqlTypeInfo::new(TypeId::FLOAT)
@@ -23,6 +36,10 @@ impl Decode<MySql> for f32 {
     }
 }
 
+/// The equivalent MySQL type for `f64` is `DOUBLE`.
+///
+/// Note that `DOUBLE` is a floating-point type and cannot represent some fractional values
+/// exactly.
 impl HasSqlType<f64> for MySql {
     fn type_info() -> MySqlTypeInfo {
         MySqlTypeInfo::new(TypeId::DOUBLE)
