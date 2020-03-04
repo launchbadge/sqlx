fe16c83b0fb91af4643e0fb6892039ad0e456cdc
Daniel Akhterov <akhterovd@gmail.com>
2020-03-04 02:58:34 -0800

    Use `Transaction` type instead of macros

--- cargo-sqlx/src/run.rs
+++ cargo-sqlx/src/run.rs
@@ -12,6 +12,7 @@ use sqlx::Connect;
 use sqlx::Connection;
 use sqlx::Database;
 use sqlx::encode::Encode;
+use sqlx::Transaction;
 use url::Url;
 use std::io;
 
@@ -94,19 +95,7 @@ impl<'a> Hash for Option<&'a Migration> {
     }
 }
 
-macro_rules! conditional_rollback {
-    ($conn: ident, $ident: ident) => {
-        match $ident {
-            Err(err) => {
-                $conn.send("rollback").await?;
-                return Err(err)?;
-            },
-            Ok(value) => value,
-        };
-    }
-}
-
-async fn migrate<C, P>(mut conn: C, path: P) -> anyhow::Result<()> 
+async fn migrate<C, P>(mut conn: C, path: P) -> anyhow::Result<()>
 where 
     C: Connection,
     <C as sqlx::Executor>::Database: Sized,
@@ -116,10 +105,9 @@ where
     String: Encode<<C as sqlx::Executor>::Database>,
     Migration: FromRow<<<C as sqlx::Executor>::Database as Database>::Row>,
     P: AsRef<Path>, {
-    conn.send("begin").await?;
+    let conn = conn.begin().await?;
 
-    let result = conn.send("create table if not exists sqlx_migrations (migration text not null primary key, hash bytea not null)").await;
-    conditional_rollback!(conn, result);
+    let result = conn.send("create table if not exists sqlx_migrations (migration text not null primary key, hash bytea not null)").await?;
 
     let mut files = fs::read_dir(path)?
         .filter_map(Result::ok)
@@ -129,8 +117,7 @@ where
     let migrations =
         sqlx::query_as::<<C as sqlx::Executor>::Database, Migration>("select * from sqlx_migrations order by migration asc")
             .fetch_all(&mut conn)
-            .await;
-    let migrations = conditional_rollback!(conn, migrations);
+            .await?;
 
     for (index, file) in files.iter().enumerate() {
         let filename = file.file_name().into_string().unwrap();
@@ -138,26 +125,23 @@ where
         let migration = fs::read_to_string(file.path());
         match (&migration, &migration.hash(), migrations.get(index).hash()) {
             (Ok(ref migration_to_run), Some(migration_to_run_hash), Some(upstream_migration_hash)) if migration_to_run_hash == upstream_migration_hash => {
-                let result = conn.send(migration_to_run).await;
-                conditional_rollback!(conn, result);
+                let result = conn.send(migration_to_run).await?;
 
                 let result = sqlx::query("INSERT INTO sqlx_migrations (migration, hash) VALUES ($1, $2)")
                     .bind(filename)
                     .bind(migration_to_run_hash)
                     .execute(&mut conn)
-                    .await;
-                conditional_rollback!(conn, result);
+                    .await?;
             }
 
             (_, _, _) => {
-                conn.send("rollback").await?;
+                conn.rollback().await?;
                 return Err(anyhow!("{:?} is not synced with the database.", filename));
             }
         }
     }
 
-    let result = conn.send("commit").await;
-    conditional_rollback!(conn, result);
+    let result = conn.commit().await?;
 
     Ok(())
 }
