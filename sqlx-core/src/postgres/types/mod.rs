7ab76ba84e9dc0d3c1f0bd5d2fde5d9d4ecfa676
PoiScript <poiscript@gmail.com>
2020-03-20 10:16:15 +0800

    make inet and cidr compatible

--- sqlx-core/src/postgres/types/mod.rs
+++ sqlx-core/src/postgres/types/mod.rs
@@ -142,8 +142,16 @@ impl Display for PgTypeInfo {
 
 impl TypeInfo for PgTypeInfo {
     fn compatible(&self, other: &Self) -> bool {
-        // TODO: 99% of postgres types are direct equality for [compatible]; when we add something that isn't (e.g, JSON/JSONB), fix this here
-        self.id.0 == other.id.0
+        match (self.id, other.id) {
+            (TypeId::CIDR, TypeId::INET)
+            | (TypeId::INET, TypeId::CIDR)
+            | (TypeId::ARRAY_CIDR, TypeId::ARRAY_INET)
+            | (TypeId::ARRAY_INET, TypeId::ARRAY_CIDR) => true,
+            _ => {
+                // TODO: 99% of postgres types are direct equality for [compatible]; when we add something that isn't (e.g, JSON/JSONB), fix this here
+                self.id.0 == other.id.0
+            }
+        }
     }
 }
 
