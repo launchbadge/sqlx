24d62dd464e4af07ee02e02c68dae34a2cbc51e2
Ryan Leckey <ryan@launchbadge.com>
2020-03-20 19:54:44 -0700

    Add DecodeOwned to help with trait bounds on owned data

--- sqlx-core/src/decode.rs
+++ sqlx-core/src/decode.rs
@@ -1,12 +1,23 @@
 //! Types and traits for decoding values from the database.
 
-use crate::database::HasRawValue;
+use crate::database::{Database, HasRawValue};
 
-/// Decode a single value from the database.
+/// A type that can be decoded from the database.
 pub trait Decode<'de, DB>
 where
     Self: Sized + 'de,
     DB: HasRawValue<'de>,
 {
     fn decode(value: DB::RawValue) -> crate::Result<Self>;
+}
+
+/// A type that can be decoded without borrowing from the connection.
+pub trait DecodeOwned<DB: Database>: for<'de> Decode<'de, DB> {}
+
+impl<DB, T> DecodeOwned<DB> for T
+where
+    DB: Database,
+    T: 'static,
+    T: for<'de> Decode<'de, DB>,
+{
 }
