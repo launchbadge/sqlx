56799c281cb8e071d5801d13b78f6d4f98399038
xiaopengli89 <x.friday@outlook.com>
2020-04-08 17:24:13 +0800

    refactor: mysql decimal value is just ascii str

--- sqlx-core/src/mysql/types/bigdecimal.rs
+++ sqlx-core/src/mysql/types/bigdecimal.rs
@@ -1,5 +1,4 @@
-use bigdecimal::{BigDecimal, Signed};
-use num_bigint::{BigInt, Sign};
+use bigdecimal::BigDecimal;
 
 use crate::decode::Decode;
 use crate::encode::Encode;
@@ -8,9 +7,7 @@ use crate::mysql::protocol::TypeId;
 use crate::mysql::{MySql, MySqlData, MySqlTypeInfo, MySqlValue};
 use crate::types::Type;
 use crate::Error;
-
-const SIGN_NEG: u8 = 0x2D;
-const SCALE_START: u8 = 0x2E;
+use std::str::FromStr;
 
 impl Type<MySql> for BigDecimal {
     fn type_info() -> MySqlTypeInfo {
@@ -21,68 +18,15 @@ impl Type<MySql> for BigDecimal {
 impl Encode<MySql> for BigDecimal {
     fn encode(&self, buf: &mut Vec<u8>) {
         let size = Encode::<MySql>::size_hint(self) - 1;
-
         assert!(size <= std::u8::MAX as usize, "Too large size");
-
         buf.push(size as u8);
-
-        if self.is_negative() {
-            buf.push(SIGN_NEG);
-        }
-
-        let (bi, scale) = self.as_bigint_and_exponent();
-        let (_, mut radix) = bi.to_radix_be(10);
-        let mut scale_index: Option<usize> = None;
-
-        if scale < 0 {
-            radix.append(&mut vec![0u8; -scale as usize]);
-        } else {
-            let scale = scale as usize;
-            if scale >= radix.len() {
-                let mut radix_temp = vec![0u8; scale - radix.len() + 1];
-                radix_temp.append(&mut radix);
-                radix = radix_temp;
-                scale_index = Some(0);
-            } else {
-                scale_index = Some(radix.len() - scale - 1);
-            }
-        }
-
-        for (i, data) in radix.iter().enumerate() {
-            buf.push(*data + 0x30);
-            if let Some(si) = scale_index {
-                if si == i {
-                    buf.push(SCALE_START);
-                    scale_index = None;
-                }
-            }
-        }
+        let s = self.to_string();
+        buf.extend_from_slice(s.as_bytes());
     }
 
-    /// 15, -2 => 1500
-    /// 15, 1 => 1.5
-    /// 15, 2 => 0.15
-    /// 15, 3 => 0.015
-
     fn size_hint(&self) -> usize {
-        let (bi, scale) = self.as_bigint_and_exponent();
-        let (_, radix) = bi.to_radix_be(10);
-        let mut s = radix.len();
-
-        if scale < 0 {
-            s = s + (-scale) as usize
-        } else if scale > 0 {
-            let scale = scale as usize;
-            if scale >= s {
-                s = scale + 1
-            }
-            s = s + 1;
-        }
-
-        if self.is_negative() {
-            s = s + 1;
-        }
-        s + 1
+        let s = self.to_string();
+        s.as_bytes().len() + 1
     }
 }
 
@@ -90,84 +34,39 @@ impl Decode<'_, MySql> for BigDecimal {
     fn decode(value: MySqlValue) -> crate::Result<Self> {
         match value.try_get()? {
             MySqlData::Binary(mut binary) => {
-                let len = binary.get_u8()?;
-                let mut negative = false;
-                let mut scale: Option<i64> = None;
-                let mut v: Vec<u8> = Vec::with_capacity(len as usize);
-
-                loop {
-                    if binary.len() < 1 {
-                        break;
-                    }
-                    let data = binary.get_u8()?;
-                    match data {
-                        SIGN_NEG => {
-                            if !negative {
-                                negative = true;
-                            } else {
-                                return Err(Error::Decode(
-                                    format!("Unexpected byte: {:X?}", data).into(),
-                                ));
-                            }
-                        }
-                        SCALE_START => {
-                            if scale.is_none() {
-                                scale = Some(0);
-                            } else {
-                                return Err(Error::Decode(
-                                    format!("Unexpected byte: {:X?}", data).into(),
-                                ));
-                            }
-                        }
-                        0x30..=0x39 => {
-                            scale = scale.map(|s| s + 1);
-                            v.push(data - 0x30);
-                        }
-                        _ => {
-                            return Err(Error::Decode(
-                                format!("Unexpected byte: {:X?}", data).into(),
-                            ))
-                        }
-                    }
-                }
-
-                let r = BigInt::from_radix_be(
-                    if negative { Sign::Minus } else { Sign::Plus },
-                    v.as_slice(),
-                    10,
-                )
-                .ok_or(Error::Decode("Can't convert to BigInt".into()))?;
-
-                Ok(BigDecimal::new(r, scale.unwrap_or(0)))
+                let _len = binary.get_u8()?;
+                let s = std::str::from_utf8(binary).map_err(Error::decode)?;
+                Ok(BigDecimal::from_str(s).map_err(Error::decode)?)
             }
-            MySqlData::Text(_) => Err(Error::Decode(
-                "`BigDecimal` can only be decoded from the binary protocol".into(),
-            )),
+            MySqlData::Text(s) => {
+                let s = std::str::from_utf8(s).map_err(Error::decode)?;
+                Ok(BigDecimal::from_str(s).map_err(Error::decode)?)
+            }
         }
     }
 }
 
 #[test]
 fn test_encode_decimal() {
-    let v = BigDecimal::new(BigInt::from(-105), 2);
+    let v: BigDecimal = BigDecimal::from_str("-1.05").unwrap();
     let mut buf: Vec<u8> = vec![];
     <BigDecimal as Encode<MySql>>::encode(&v, &mut buf);
-    assert_eq!(buf, vec![0x05, 0x2D, 0x31, 0x2E, 0x30, 0x35]);
+    assert_eq!(buf, vec![0x05, b'-', b'1', b'.', b'0', b'5']);
 
-    let v = BigDecimal::new(BigInt::from(-105), -3);
+    let v: BigDecimal = BigDecimal::from_str("-105000").unwrap();
     let mut buf: Vec<u8> = vec![];
     <BigDecimal as Encode<MySql>>::encode(&v, &mut buf);
-    assert_eq!(buf, vec![0x07, 0x2D, 0x31, 0x30, 0x35, 0x30, 0x30, 0x30]);
+    assert_eq!(buf, vec![0x07, b'-', b'1', b'0', b'5', b'0', b'0', b'0']);
 
-    let v = BigDecimal::new(BigInt::from(105), 5);
+    let v: BigDecimal = BigDecimal::from_str("0.00105").unwrap();
     let mut buf: Vec<u8> = vec![];
     <BigDecimal as Encode<MySql>>::encode(&v, &mut buf);
-    assert_eq!(buf, vec![0x07, 0x30, 0x2E, 0x30, 0x30, 0x31, 0x30, 0x35]);
+    assert_eq!(buf, vec![0x07, b'0', b'.', b'0', b'0', b'1', b'0', b'5']);
 }
 
 #[test]
 fn test_decode_decimal() {
-    let buf: Vec<u8> = vec![0x05, 0x2D, 0x31, 0x2E, 0x30, 0x35];
+    let buf: Vec<u8> = vec![0x05, b'-', b'1', b'.', b'0', b'5'];
     let v = <BigDecimal as Decode<'_, MySql>>::decode(MySqlValue::binary(
         MySqlTypeInfo::new(TypeId::NEWDECIMAL),
         buf.as_slice(),
@@ -175,7 +74,7 @@ fn test_decode_decimal() {
     .unwrap();
     assert_eq!(v.to_string(), "-1.05");
 
-    let buf: Vec<u8> = vec![0x04, 0x30, 0x2E, 0x30, 0x35];
+    let buf: Vec<u8> = vec![0x04, b'0', b'.', b'0', b'5'];
     let v = <BigDecimal as Decode<'_, MySql>>::decode(MySqlValue::binary(
         MySqlTypeInfo::new(TypeId::NEWDECIMAL),
         buf.as_slice(),
@@ -183,7 +82,7 @@ fn test_decode_decimal() {
     .unwrap();
     assert_eq!(v.to_string(), "0.05");
 
-    let buf: Vec<u8> = vec![0x06, 0x2D, 0x39, 0x30, 0x30, 0x30, 0x30];
+    let buf: Vec<u8> = vec![0x06, b'-', b'9', b'0', b'0', b'0', b'0'];
     let v = <BigDecimal as Decode<'_, MySql>>::decode(MySqlValue::binary(
         MySqlTypeInfo::new(TypeId::NEWDECIMAL),
         buf.as_slice(),
