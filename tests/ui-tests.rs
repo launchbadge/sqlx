a7d039931c4a4dacaa000f3f008656c332304881
Austin Bonander <austin@launchbadge.com>
2020-04-27 18:49:40 -0700

    fix UI tests
    looks like the `.stderr` file for a SQLite UI test never got added, and notes on errors in other tests changed slightly

--- tests/ui-tests.rs
+++ tests/ui-tests.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 #[test]
 fn ui_tests() {
     let t = trybuild::TestCases::new();
@@ -29,6 +31,13 @@ fn ui_tests() {
     }
 
     if cfg!(feature = "sqlite") {
+        if dotenv::var("DATABASE_URL").map_or(true, |v| {
+            Path::is_relative(v.trim_start_matches("sqlite://").as_ref())
+        }) {
+            // this isn't `Trybuild`'s fault: https://github.com/dtolnay/trybuild/issues/69#issuecomment-620329526
+            panic!("DATABASE_URL must contain an absolute path for SQLite UI tests")
+        }
+
         t.compile_fail("tests/ui/sqlite/*.rs");
     }
 
