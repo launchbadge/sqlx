0b3d24fd57a0bf5eba87bed407158dfbdd7d755e
Julius de Bruijn <julius+github@nauk.io>
2020-06-26 10:23:17 +0200

    Remove warnings on mssql for statement cache

--- sqlx-core/src/common/mod.rs
+++ sqlx-core/src/common/mod.rs
@@ -1,3 +1,5 @@
+#[cfg(any(feature = "sqlite", feature = "postgres", feature = "mysql"))]
 mod statement_cache;
 
+#[cfg(any(feature = "sqlite", feature = "postgres", feature = "mysql"))]
 pub(crate) use statement_cache::StatementCache;
