6235eb5a5e15cba863cdea8402986ee16eeb5fcf
Ryan Leckey <ryan@launchbadge.com>
2020-06-27 04:09:58 -0700

    fix: StatementCache::capacity is only used by sqlite

--- sqlx-core/src/common/statement_cache.rs
+++ sqlx-core/src/common/statement_cache.rs
@@ -60,6 +60,7 @@ impl<T> StatementCache<T> {
     }
 
     /// Returns the maximum number of statements the cache can hold.
+    #[cfg(any(feature = "sqlite"))]
     pub fn capacity(&self) -> usize {
         self.inner.capacity()
     }
