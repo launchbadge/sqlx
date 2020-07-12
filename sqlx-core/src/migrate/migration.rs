74f3cad8b6385f3e2316a9739551c6b15c91896f
Kaleb Elwert <belak@coded.io>
2020-07-12 12:32:56 -0700

    Allow MigrationSource to be implemented externally

--- sqlx-core/src/migrate/migration.rs
+++ sqlx-core/src/migrate/migration.rs
@@ -1,9 +1,24 @@
 use std::borrow::Cow;
 
+use sha2::{Digest, Sha384};
+
 #[derive(Debug, Clone)]
 pub struct Migration {
     pub version: i64,
     pub description: Cow<'static, str>,
     pub sql: Cow<'static, str>,
     pub checksum: Cow<'static, [u8]>,
+}
+
+impl Migration {
+    pub fn new(version: i64, description: Cow<'static, str>, sql: Cow<'static, str>) -> Self {
+        let checksum = Cow::Owned(Vec::from(Sha384::digest(sql.as_bytes()).as_slice()));
+
+        Migration {
+            version,
+            description,
+            sql,
+            checksum,
+        }
+    }
 }
