d04d612368adcc6d300af6a0539ee1334655fd5d
Ryan Leckey <ryan@launchbadge.com>
2020-07-27 02:22:31 -0700

    Remove realworld example.
    I'd prefer to have one but I'd rather not have one then one that doesn't compile.
    
    Ideally someone could take up maintaining a realworld example outside of SQLx. Until GATs though we
    should keep this to one database. Looking back, most of the reason this is so hard to migrate to 0.4 is
    because of how generic the bounds are.

--- examples/realworld/src/lib.rs
+++
@@ -1,8 +0,0 @@
-/// Biz logic for the RealWorld backend API
-///
-/// See the [RealWorld API Spec](https://github.com/gothinkster/realworld/tree/master/api) for
-/// more information on the API
-pub mod api;
-
-/// Database models and connectors
-pub mod db;
