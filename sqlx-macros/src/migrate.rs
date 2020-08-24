ef313f0611b65cbf407b68c7da586990a0325d40
sid <sidred@gmail.com>
2020-08-24 23:59:38 +0530

    fix migrate marco to take migration type

--- sqlx-macros/src/migrate.rs
+++ sqlx-macros/src/migrate.rs
@@ -1,12 +1,29 @@
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens, TokenStreamExt};
 use sha2::{Digest, Sha384};
+use sqlx_core::migrate::MigrationType;
 use std::fs;
 use syn::LitStr;
+
+pub struct QuotedMigrationType(MigrationType);
+
+impl ToTokens for QuotedMigrationType {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let ts = match self.0 {
+            MigrationType::Simple => quote! { sqlx::migrate::MigrationType::Simple },
+            MigrationType::ReversibleUp => quote! { sqlx::migrate::MigrationType::ReversibleUp },
+            MigrationType::ReversibleDown => {
+                quote! { sqlx::migrate::MigrationType::ReversibleDown }
+            }
+        };
+        tokens.append_all(ts.into_iter());
+    }
+}
 
 struct QuotedMigration {
     version: i64,
     description: String,
+    migration_type: QuotedMigrationType,
     sql: String,
     checksum: Vec<u8>,
 }
@@ -16,6 +33,7 @@ impl ToTokens for QuotedMigration {
         let QuotedMigration {
             version,
             description,
+            migration_type,
             sql,
             checksum,
         } = &self;
@@ -24,6 +42,7 @@ impl ToTokens for QuotedMigration {
             sqlx::migrate::Migration {
                 version: #version,
                 description: std::borrow::Cow::Borrowed(#description),
+                migration_type:  #migration_type,
                 sql: std::borrow::Cow::Borrowed(#sql),
                 checksum: std::borrow::Cow::Borrowed(&[
                     #(#checksum),*
@@ -59,9 +78,10 @@ pub(crate) fn expand_migrator_from_dir(dir: LitStr) -> crate::Result<proc_macro2
 
         let version: i64 = parts[0].parse()?;
 
+        let migration_type = MigrationType::from_filename(parts[1]);
         // remove the `.sql` and replace `_` with ` `
         let description = parts[1]
-            .trim_end_matches(".sql")
+            .trim_end_matches(migration_type.suffix())
             .replace('_', " ")
             .to_owned();
 
@@ -72,6 +92,7 @@ pub(crate) fn expand_migrator_from_dir(dir: LitStr) -> crate::Result<proc_macro2
         migrations.push(QuotedMigration {
             version,
             description,
+            migration_type: QuotedMigrationType(migration_type),
             sql,
             checksum,
         })
