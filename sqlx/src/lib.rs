8276f88df34bf8eaf44892e739b173985614924b
Ryan Leckey <ryan@launchbadge.com>
2020-12-27 12:26:48 -0800

    chore: initial commit

---
+++ sqlx/src/lib.rs
@@ -0,0 +1,17 @@
+//! SQLx is an async, pure Rust SQL crate featuring compile-time checked queries without a DSL.
+//!
+#![deny(unsafe_code)]
+#![warn(rust_2018_idioms)]
+#![warn(future_incompatible)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::cargo_common_metadata)]
+#![warn(clippy::multiple_crate_versions)]
+#![warn(clippy::cognitive_complexity)]
+#![warn(clippy::future_not_send)]
+#![warn(clippy::missing_const_for_fn)]
+#![warn(clippy::needless_borrow)]
+#![warn(clippy::redundant_pub_crate)]
+#![warn(clippy::string_lit_as_bytes)]
+#![warn(clippy::use_self)]
+#![warn(clippy::useless_let_if_seq)]
+#![allow(clippy::clippy::doc_markdown)]
