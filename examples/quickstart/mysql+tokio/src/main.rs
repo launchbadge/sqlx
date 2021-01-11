e1e6f594c1c408f71fd1270e56ff34e1fd90317e
Ryan Leckey <ryan@launchbadge.com>
2021-01-10 19:52:50 -0800

    docs: split quickstart into mysql+tokio, mysql+async-std, and mysql+blocking

---
+++ examples/quickstart/mysql+tokio/src/main.rs
@@ -0,0 +1,26 @@
+use sqlx::mysql::{MySqlConnectOptions, MySqlConnection};
+use sqlx::prelude::*;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::try_init()?;
+
+    // parse the connection URL
+    let mut conn: MySqlConnection = MySqlConnectOptions::parse("mysql://root@localhost")?
+        // set a password (perhaps from somewhere else than the rest of the URL)
+        .password("password")
+        // connect to the database (asynchronously)
+        .connect()
+        .await?;
+
+    // ping, this makes sure the server is still there
+    // hopefully it is – we did just connect to it
+    conn.ping().await?;
+
+    // close the connection explicitly
+    // this kindly informs the database server that we'll be terminating
+    // while not strictly required, the server will dispose of connection resources faster
+    conn.close().await?;
+
+    Ok(())
+}
