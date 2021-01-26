66ceff37b7b6593284810e8291474c3262c6b5c0
Ryan Leckey <ryan@launchbadge.com>
2021-01-26 01:03:49 -0800

    feat(core): impl io::Serialize for a byte slice

--- sqlx-core/src/io/serialize.rs
+++ sqlx-core/src/io/serialize.rs
@@ -9,3 +9,11 @@ pub trait Serialize<'ser, Cx = ()>: Sized {
 
     fn serialize_with(&self, buf: &mut Vec<u8>, context: Cx) -> crate::Result<()>;
 }
+
+impl<Cx> Serialize<'_, Cx> for &'_ [u8] {
+    fn serialize_with(&self, buf: &mut Vec<u8>, _: Cx) -> crate::Result<()> {
+        buf.extend_from_slice(*self);
+
+        Ok(())
+    }
+}
