2fd26b5504433fbe6873c807521e3d48f7dbc6bb
Ryan Leckey <ryan@launchbadge.com>
2021-02-04 06:17:17 -0800

    sqlite: remove support for u64
    see https://github.com/launchbadge/sqlx/pull/919#discussion_r557106441

--- sqlx-core/src/sqlite/types/uint.rs
+++ sqlx-core/src/sqlite/types/uint.rs
@@ -78,27 +78,3 @@ impl<'r> Decode<'r, Sqlite> for u32 {
         Ok(value.int64().try_into()?)
     }
 }
-
-impl Type<Sqlite> for u64 {
-    fn type_info() -> SqliteTypeInfo {
-        SqliteTypeInfo(DataType::Int64)
-    }
-
-    fn compatible(ty: &SqliteTypeInfo) -> bool {
-        matches!(ty.0, DataType::Int | DataType::Int64)
-    }
-}
-
-impl<'q> Encode<'q, Sqlite> for u64 {
-    fn encode_by_ref(&self, args: &mut Vec<SqliteArgumentValue<'q>>) -> IsNull {
-        args.push(SqliteArgumentValue::Int64(*self as i64));
-
-        IsNull::No
-    }
-}
-
-impl<'r> Decode<'r, Sqlite> for u64 {
-    fn decode(value: SqliteValueRef<'r>) -> Result<Self, BoxDynError> {
-        Ok(value.int64().try_into()?)
-    }
-}
