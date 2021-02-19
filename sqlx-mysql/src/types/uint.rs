924340c5b4018e6c31a37ed45e16f5d29b8a979e
Ryan Leckey <ryan@launchbadge.com>
2021-02-18 21:43:58 -0800

    feat(mysql): handle conversions from other integer types in u8

--- sqlx-mysql/src/types/uint.rs
+++ sqlx-mysql/src/types/uint.rs
@@ -1,18 +1,77 @@
+use std::cmp;
+use std::convert::{TryFrom, TryInto};
+use std::error::Error as StdError;
+use std::num::TryFromIntError;
+use std::str::FromStr;
+
 use bytes::Buf;
-use sqlx_core::{decode, encode};
-use sqlx_core::{Decode, Encode};
+use sqlx_core::{decode, encode, Database, TypeEncode};
+use sqlx_core::{Decode, Encode, Type};
 
 use crate::type_info::MySqlTypeInfo;
 use crate::MySqlRawValueFormat::*;
-use crate::{MySql, MySqlOutput, MySqlRawValue};
+use crate::{MySql, MySqlOutput, MySqlRawValue, MySqlRawValueFormat, MySqlTypeId};
 
 // https://dev.mysql.com/doc/internals/en/binary-protocol-value.html#packet-ProtocolBinary
 
-// TODO: accepts(ty) -> ty.is_integer()
-// TODO: compatible(ty) -> ty.is_integer()
+const NUMBER_TOO_LARGE: &str = "number too large to fit in target type";
+
+// shared among all Decode impls for unsigned and signed integers
+fn decode_int_or_uint<T>(value: MySqlRawValue<'_>) -> decode::Result<T>
+where
+    T: TryFrom<i64> + TryFrom<u64> + FromStr,
+    <T as TryFrom<i64>>::Error: 'static + StdError + Send + Sync,
+    <T as TryFrom<u64>>::Error: 'static + StdError + Send + Sync,
+    <T as FromStr>::Err: 'static + StdError + Send + Sync,
+{
+    if value.format() == MySqlRawValueFormat::Text {
+        return Ok(value.as_str()?.parse()?);
+    }
+
+    let mut bytes = value.as_bytes()?;
+
+    // start from u64 if the value is marked as unsigned
+    // otherwise start from i64
+    let is_unsigned = value.type_info().id().is_unsigned();
+
+    // pull at most 8 bytes from the buffer
+    let len = cmp::max(bytes.len(), 8);
+
+    Ok(if is_unsigned {
+        bytes.get_uint_le(len).try_into()?
+    } else {
+        bytes.get_int_le(len).try_into()?
+    })
+}
+
+impl Type<MySql> for u8 {
+    fn type_id() -> MySqlTypeId {
+        MySqlTypeId::TINYINT_UNSIGNED
+    }
+
+    fn compatible(ty: &MySqlTypeInfo) -> bool {
+        ty.id().is_integer()
+    }
+}
+
+impl TypeEncode<MySql> for u8 {
+    fn type_id(&self, _: &MySqlTypeInfo) -> MySqlTypeId {
+        <u8 as Type<MySql>>::type_id()
+    }
+}
 
 impl Encode<MySql> for u8 {
-    fn encode(&self, _: &MySqlTypeInfo, out: &mut MySqlOutput<'_>) -> encode::Result<()> {
+    fn encode(&self, ty: &MySqlTypeInfo, out: &mut MySqlOutput<'_>) -> encode::Result<()> {
+        match ty.id() {
+            MySqlTypeId::TINYINT_UNSIGNED => {}
+
+            MySqlTypeId::TINYINT if *self > 0x7f => {
+                return Err(encode::Error::msg(NUMBER_TOO_LARGE));
+            }
+
+            _ => {}
+        }
+
         out.buffer().push(*self);
 
         Ok(())
@@ -21,11 +80,6 @@ impl Encode<MySql> for u8 {
 
 impl<'r> Decode<'r, MySql> for u8 {
     fn decode(value: MySqlRawValue<'r>) -> decode::Result<Self> {
-        // FIXME: ensure that the SQL value fits within u8
-
-        Ok(match value.format() {
-            Binary => value.as_bytes()?.get_u8(),
-            Text => value.as_str()?.parse()?,
-        })
+        decode_int_or_uint(value)
     }
 }
