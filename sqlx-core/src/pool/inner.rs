72854b193dd5b1fb0e6ed48135c3299ae3388c1d
rich-murphey <rich@murphey.org>
2021-03-03 02:36:30 -0600

    use match per @abonander

--- sqlx-core/src/pool/inner.rs
+++ sqlx-core/src/pool/inner.rs
@@ -113,14 +113,12 @@ impl<DB: Database> SharedPool<DB> {
         let mut size = self.size();
 
         while size < self.options.max_connections {
-            if let Ok(new_size) =
-                self.size
-                    .compare_exchange(size, size + 1, Ordering::AcqRel, Ordering::Acquire)
+            match self
+                .size
+                .compare_exchange(size, size + 1, Ordering::AcqRel, Ordering::Acquire)
             {
-                if new_size == size {
-                    return Some(DecrementSizeGuard::new(self));
-                }
-                size = new_size;
+                Ok(_) => return Some(DecrementSizeGuard::new(self)),
+                Err(new_size) => size = new_size,
             }
         }
 
