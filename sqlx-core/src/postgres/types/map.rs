d1c927946fc7fc3b9800f5a77d97679ffda4c282
Luc Perkins <luc@timber.io>
2021-03-04 09:32:10 -0800

    Remove array and vec impls
    Signed-off-by: Luc Perkins <luc@timber.io>

--- sqlx-core/src/postgres/types/map.rs
+++ sqlx-core/src/postgres/types/map.rs
@@ -6,6 +6,8 @@ use crate::postgres::{PgArgumentBuffer, PgTypeInfo, PgValueFormat, PgValueRef, P
 use crate::types::Type;
 use serde_json::{Map, Value};
 
+// This provides a `Type` implementation for cases when you specifically need a JSON object
+// and not another JSON-compatible value (i.e. not an array, Boolean, string, etc.).
 impl Type<Postgres> for Map<String, Value> {
     fn type_info() -> PgTypeInfo {
         PgTypeInfo::JSONB
@@ -13,26 +15,6 @@ impl Type<Postgres> for Map<String, Value> {
 
     fn compatible(ty: &PgTypeInfo) -> bool {
         *ty == PgTypeInfo::JSON || *ty == PgTypeInfo::JSONB
-    }
-}
-
-impl Type<Postgres> for [Map<String, Value>] {
-    fn type_info() -> PgTypeInfo {
-        PgTypeInfo::JSONB_ARRAY
-    }
-
-    fn compatible(ty: &PgTypeInfo) -> bool {
-        array_compatible::<Map<String, Value>>(ty)
-    }
-}
-
-impl Type<Postgres> for Vec<Map<String, Value>> {
-    fn type_info() -> PgTypeInfo {
-        <[Map<String, Value>] as Type<Postgres>>::type_info()
-    }
-
-    fn compatible(ty: &PgTypeInfo) -> bool {
-        <[Map<String, Value>] as Type<Postgres>>::compatible(ty)
     }
 }
 
