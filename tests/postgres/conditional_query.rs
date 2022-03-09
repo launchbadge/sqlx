d45d86b8e3f3a8cb449c8fd96435be69eafe9395
Moritz Bischof <moritz.bischof1@gmail.com>
2022-03-09 13:52:36 +0100

    tests: add dynamic_filtering test

--- tests/postgres/conditional_query.rs
+++ tests/postgres/conditional_query.rs
@@ -1,4 +1,4 @@
-use sqlx_core::postgres::Postgres;
+use sqlx_core::postgres::{PgConnection, Postgres};
 use sqlx_test::new;
 
 #[sqlx_macros::test]
@@ -30,7 +30,7 @@ async fn simple() -> anyhow::Result<()> {
 }
 
 #[sqlx_macros::test]
-async fn single_if() -> anyhow::Result<()> {
+async fn dynamic_ordering() -> anyhow::Result<()> {
     let mut conn = new::<Postgres>().await?;
 
     #[derive(Clone, Eq, PartialEq, Debug)]
@@ -83,6 +83,61 @@ async fn single_if() -> anyhow::Result<()> {
 }
 
 #[sqlx_macros::test]
-async fn fail() -> anyhow::Result<()> {
-    panic!("test this gets executed");
+async fn dynamic_filtering() -> anyhow::Result<()> {
+    let mut conn = new::<Postgres>().await?;
+
+    #[derive(Clone, Eq, PartialEq, Debug)]
+    struct Article {
+        id: i32,
+        title: String,
+        author: String,
+    }
+
+    enum Filter {
+        Id(i32),
+        Title(String),
+        Author(String),
+    }
+
+    async fn query_articles(
+        con: &mut PgConnection,
+        filter: Option<Filter>,
+    ) -> anyhow::Result<Vec<Article>> {
+        let articles = sqlx::query_as!(
+            Article,
+            "SELECT"
+                r#"id AS "id!", title AS "title!", author AS "author!""#
+            "FROM ("
+                "VALUES (1, 'Article1', 'Peter'), (2, 'Article2', 'John'), (3, 'Article3', 'James')"
+            ") articles(id, title, author)"
+            if let Some(filter) = filter {
+                "WHERE"
+                match filter {
+                    Filter::Id(id) => "id = {id}",
+                    Filter::Title(title) => "title ILIKE {title}",
+                    Filter::Author(author) => "author ILIKE {author}"
+                }
+            }
+        )
+        .fetch_all(&mut conn)
+        .await?;
+        Ok(articles)
+    }
+
+    let result = query_articles(&mut conn, None).await?;
+    assert_eq!(result.len(), 3);
+
+    let result = query_articles(&mut conn, Some(Filter::Id(1))).await?;
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].id, 1);
+
+    let result = query_articles(&mut conn, Some(Filter::Title("article2".to_owned()))).await?;
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].id, 2);
+
+    let result = query_articles(&mut conn, Some(Filter::Author("james".to_owned()))).await?;
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].id, 3);
+
+    Ok(())
 }
