cc1ab90110e624f469ba4215c70273bc947b20f4
Pruthvikar Reddy <2778014+pruthvikar@users.noreply.github.com>
2022-04-15 00:05:38 +0200

    Fix Rust type for SQLite REAL (#1785)
    REAL is stored as as 8-byte IEEE floating point number
    See https://www.sqlite.org/datatype3.html#storage_classes_and_datatypes

--- sqlx-macros/src/database/sqlite.rs
+++ sqlx-macros/src/database/sqlite.rs
@@ -1,11 +1,13 @@
 use sqlx_core as sqlx;
 
+// f32 is not included below as REAL represents a floating point value
+// stored as an 8-byte IEEE floating point number
+// For more info see: https://www.sqlite.org/datatype3.html#storage_classes_and_datatypes
 impl_database_ext! {
     sqlx::sqlite::Sqlite {
         bool,
         i32,
         i64,
-        f32,
         f64,
         String,
         Vec<u8>,
