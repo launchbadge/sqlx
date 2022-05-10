6284922c37b5ceaa54e1b662a05fe256e3fd9db6
Erik Rhodes <erik@space-nav.com>
2022-05-10 14:05:05 -0600

    fixed issue with comparing ref

--- sqlx-core/src/mysql/connection/stream.rs
+++ sqlx-core/src/mysql/connection/stream.rs
@@ -148,7 +148,7 @@ impl MySqlStream {
         // TODO: packet compression
         // TODO: packet joining
 
-        if payload.get(0).ok_or(err_protocol!("Packet empty"))? == 0xff {
+        if payload.get(0).ok_or(err_protocol!("Packet empty"))?.eq(&0xff) {
             self.waiting.pop_front();
 
             // instead of letting this packet be looked at everywhere, we check here
