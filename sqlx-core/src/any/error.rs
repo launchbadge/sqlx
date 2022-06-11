1a2cc7b9f1755b23059e985224a765d4ba5008ee
Yota Toyama <raviqqe@gmail.com>
2022-06-11 21:56:13 +0000

    Refactor

--- sqlx-core/src/any/error.rs
+++ sqlx-core/src/any/error.rs
@@ -1,12 +1,12 @@
 use std::any::type_name;
 
+use crate::any::type_info::AnyTypeInfo;
 use crate::any::Any;
-use crate::database::Database;
 use crate::error::BoxDynError;
 use crate::type_info::TypeInfo;
 use crate::types::Type;
 
-pub(super) fn mismatched_types<T: Type<Any>>(ty: &<Any as Database>::TypeInfo) -> BoxDynError {
+pub(super) fn mismatched_types<T: Type<Any>>(ty: &AnyTypeInfo) -> BoxDynError {
     format!(
         "mismatched types; Rust type `{}` is not compatible with SQL type `{}`",
         type_name::<T>(),
