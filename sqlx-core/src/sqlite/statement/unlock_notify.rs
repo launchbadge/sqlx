c6f8a412677fb192e721a738b1582b9aec961137
Austin Bonander <austin@launchbadge.com>
2023-03-21 12:01:53 -0700

    fix: non-binding `let` in SQLite driver

--- sqlx-core/src/sqlite/statement/unlock_notify.rs
+++ sqlx-core/src/sqlite/statement/unlock_notify.rs
@@ -48,7 +48,7 @@ impl Notify {
     }
 
     fn wait(&self) {
-        let _ = self
+        let _guard = self
             .condvar
             .wait_while(self.mutex.lock().unwrap(), |fired| !*fired)
             .unwrap();
