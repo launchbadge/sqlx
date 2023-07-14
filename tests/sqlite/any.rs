3662bdab8477c70847583b4ec4d69112b484bc5a
Luiz Carvalho <luizcmpc@gmail.com>
2023-07-14 20:27:53 -0300

    fix(sqlite): encode bool as integer (#2620)

---
+++ tests/sqlite/any.rs
@@ -0,0 +1,19 @@
+use sqlx::{Any, Sqlite};
+use sqlx_test::new;
+
+#[sqlx_macros::test]
+async fn it_encodes_bool_with_any() -> anyhow::Result<()> {
+    sqlx::any::install_default_drivers();
+    let mut conn = new::<Any>().await?;
+
+    let res = sqlx::query("INSERT INTO accounts VALUES (?, ?, ?)")
+        .bind(87)
+        .bind("Harrison Ford")
+        .bind(true)
+        .execute(&mut conn)
+        .await
+        .expect("failed to encode bool");
+    assert_eq!(res.rows_affected(), 1);
+
+    Ok(())
+}
