42e6fd785d0c77d357106679126b28abe0af260c
lovasoa <pere.jobs@gmail.com>
2023-07-27 08:14:46 +0200

    update webpki-roots

--- sqlx-core/src/net/tls/rustls.rs
+++ sqlx-core/src/net/tls/rustls.rs
@@ -22,7 +22,7 @@ pub async fn configure_tls_connector(
             .with_no_client_auth()
     } else {
         let mut cert_store = RootCertStore::empty();
-        cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+        cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
             OwnedTrustAnchor::from_subject_spki_name_constraints(
                 ta.subject,
                 ta.spki,
