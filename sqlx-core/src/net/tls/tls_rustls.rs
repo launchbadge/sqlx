83ca61b84f5d6f509639cb79b281624f0fdb212e
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-10-23 10:16:42 +0200

    chore: bump webpki-roots to 0.25

--- sqlx-core/src/net/tls/tls_rustls.rs
+++ sqlx-core/src/net/tls/tls_rustls.rs
@@ -109,7 +109,7 @@ where
         }
     } else {
         let mut cert_store = RootCertStore::empty();
-        cert_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+        cert_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
             OwnedTrustAnchor::from_subject_spki_name_constraints(
                 ta.subject,
                 ta.spki,
