fb942ff65b7ac7732fe1d62c14a3b548dee3849d
Jaime Liz <lizjaime@gmail.com>
2024-04-06 11:56:14 -0500

    fix: messed up merge

--- sqlx-postgres/src/migrate.rs
+++ sqlx-postgres/src/migrate.rs
@@ -208,20 +208,19 @@ CREATE TABLE IF NOT EXISTS _sqlx_migrations (
         migration: &'m Migration,
     ) -> BoxFuture<'m, Result<Duration, MigrateError>> {
         Box::pin(async move {
-            let mut tx = self.begin().await?;
             let start = Instant::now();
             if !migration.no_tx {
                 let mut tx = self.begin().await?;
 
-            // Use a single transaction for the actual migration script and the essential bookeeping so we never
-            // execute migrations twice. See https://github.com/launchbadge/sqlx/issues/1966.
-            // The `execution_time` however can only be measured for the whole transaction. This value _only_ exists for
-            // data lineage and debugging reasons, so it is not super important if it is lost. So we initialize it to -1
-            // and update it once the actual transaction completed.
-            let _ = tx
-                .execute(&*migration.sql)
-                .await
-                .map_err(|e| MigrateError::ExecuteMigration(e, migration.version))?;
+                // Use a single transaction for the actual migration script and the essential bookeeping so we never
+                // execute migrations twice. See https://github.com/launchbadge/sqlx/issues/1966.
+                // The `execution_time` however can only be measured for the whole transaction. This value _only_ exists for
+                // data lineage and debugging reasons, so it is not super important if it is lost. So we initialize it to -1
+                // and update it once the actual transaction completed.
+                let _ = tx
+                    .execute(&*migration.sql)
+                    .await
+                    .map_err(|e| MigrateError::ExecuteMigration(e, migration.version))?;
 
                 // language=SQL
                 let _ = query(
