5d97b6842efc27148cae5e7920169b7cfb342a14
Austin Bonander <austin.bonander@gmail.com>
2024-07-26 15:10:31 -0700

    chore: deprecate `AnyKind`

--- sqlx-core/src/any/kind.rs
+++ sqlx-core/src/any/kind.rs
@@ -1,6 +1,7 @@
 use crate::error::Error;
 use std::str::FromStr;
 
+#[deprecated = "not used or returned by any API"]
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum AnyKind {
     #[cfg(feature = "postgres")]
