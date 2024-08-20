445b7895ad113ff2e2528079023809b53dd0e134
Austin Bonander <austin.bonander@gmail.com>
2024-08-20 03:04:49 -0700

    fix(sqlite): audit for bad casts

--- sqlx-sqlite/src/connection/collation.rs
+++ sqlx-sqlite/src/connection/collation.rs
@@ -127,13 +127,23 @@ where
     C: Fn(&str, &str) -> Ordering,
 {
     let boxed_f: *mut C = data as *mut C;
-    debug_assert!(!boxed_f.is_null());
+
+    // Note: unwinding is now caught at the FFI boundary:
+    // https://doc.rust-lang.org/nomicon/ffi.html#ffi-and-unwinding
+    assert!(!boxed_f.is_null());
+
+    let left_len =
+        usize::try_from(left_len).unwrap_or_else(|_| panic!("left_len out of range: {left_len}"));
+
+    let right_len = usize::try_from(right_len)
+        .unwrap_or_else(|_| panic!("right_len out of range: {right_len}"));
+
     let s1 = {
-        let c_slice = slice::from_raw_parts(left_ptr as *const u8, left_len as usize);
+        let c_slice = slice::from_raw_parts(left_ptr as *const u8, left_len);
         from_utf8_unchecked(c_slice)
     };
     let s2 = {
-        let c_slice = slice::from_raw_parts(right_ptr as *const u8, right_len as usize);
+        let c_slice = slice::from_raw_parts(right_ptr as *const u8, right_len);
         from_utf8_unchecked(c_slice)
     };
     let t = (*boxed_f)(s1, s2);
--- sqlx-sqlite/src/logger.rs
+++ sqlx-sqlite/src/logger.rs
@@ -1,3 +1,11 @@
+// Bad casts in this module SHOULD NOT result in a SQL injection
+// https://github.com/launchbadge/sqlx/issues/3440
+#![allow(
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_sign_loss
+)]
+
 use crate::connection::intmap::IntMap;
 use std::collections::HashSet;
 use std::fmt::Debug;
