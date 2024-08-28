845cadd82374e3c063d34b9e7fee9dd97f24353a
James Holman <jameswarringtonholman@gmail.com>
2024-08-28 20:49:04 +1000

    fix: box

--- sqlx-postgres/src/types/geometry/box.rs
+++ sqlx-postgres/src/types/geometry/box.rs
@@ -3,10 +3,11 @@ use crate::encode::{Encode, IsNull};
 use crate::error::BoxDynError;
 use crate::types::Type;
 use crate::{PgArgumentBuffer, PgHasArrayType, PgTypeInfo, PgValueFormat, PgValueRef, Postgres};
+use sqlx_core::bytes::Buf;
 use sqlx_core::Error;
 use std::str::FromStr;
 
-const BYTE_WIDTH: usize = 8;
+const ERROR: &str = "error decoding BOX";
 
 /// Postgres Geometric Box type
 ///
@@ -39,7 +40,7 @@ impl<'r> Decode<'r, Postgres> for PgBox {
     fn decode(value: PgValueRef<'r>) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
         match value.format() {
             PgValueFormat::Text => Ok(PgBox::from_str(value.as_str()?)?),
-            PgValueFormat::Binary => Ok(pg_box_from_bytes(value.as_bytes()?)?),
+            PgValueFormat::Binary => Ok(PgBox::from_bytes(value.as_bytes()?)?),
         }
     }
 }
@@ -62,33 +63,48 @@ impl FromStr for PgBox {
         let sanitised = s.replace(&['(', ')', '[', ']', ' '][..], "");
         let mut parts = sanitised.splitn(4, ",");
 
-        if let (Some(x1_str), Some(y1_str), Some(x2_str), Some(y2_str)) =
-            (parts.next(), parts.next(), parts.next(), parts.next())
-        {
-            let x1 = parse_float_from_str(x1_str, "could not get x1")?;
-            let y1 = parse_float_from_str(y1_str, "could not get y1")?;
-            let x2 = parse_float_from_str(x2_str, "could not get x2")?;
-            let y2 = parse_float_from_str(y2_str, "could not get y2")?;
+        let x1 = parts
+            .next()
+            .and_then(|s| s.parse::<f64>().ok())
+            .ok_or(Error::Decode(
+                format!("{}: could not get x1 from {}", ERROR, s).into(),
+            ))?;
 
-            return Ok(PgBox { x1, y1, x2, y2 });
-        }
+        let y1 = parts
+            .next()
+            .and_then(|s| s.parse::<f64>().ok())
+            .ok_or(Error::Decode(
+                format!("{}: could not get y1 from {}", ERROR, s).into(),
+            ))?;
 
-        Err(Error::Decode(
-            format!("could not get x1, y1, x2, y2 from {}", s).into(),
-        ))
+        let x2 = parts
+            .next()
+            .and_then(|s| s.parse::<f64>().ok())
+            .ok_or(Error::Decode(
+                format!("{}: could not get x2 from {}", ERROR, s).into(),
+            ))?;
+
+        let y2 = parts
+            .next()
+            .and_then(|s| s.parse::<f64>().ok())
+            .ok_or(Error::Decode(
+                format!("{}: could not get y2 from {}", ERROR, s).into(),
+            ))?;
+
+        Ok(PgBox { x1, y1, x2, y2 })
     }
 }
 
-fn pg_box_from_bytes(bytes: &[u8]) -> Result<PgBox, Error> {
-    let x1 = get_f64_from_bytes(bytes, 0)?;
-    let y1 = get_f64_from_bytes(bytes, BYTE_WIDTH)?;
-    let x2 = get_f64_from_bytes(bytes, BYTE_WIDTH * 2)?;
-    let y2 = get_f64_from_bytes(bytes, BYTE_WIDTH * 3)?;
-
-    Ok(PgBox { x1, y1, x2, y2 })
-}
-
 impl PgBox {
+    fn from_bytes(mut bytes: &[u8]) -> Result<PgBox, Error> {
+        let x1 = bytes.get_f64();
+        let y1 = bytes.get_f64();
+        let x2 = bytes.get_f64();
+        let y2 = bytes.get_f64();
+
+        Ok(PgBox { x1, y1, x2, y2 })
+    }
+
     fn serialize(&self, buff: &mut PgArgumentBuffer) -> Result<(), Error> {
         buff.extend_from_slice(&self.x1.to_be_bytes());
         buff.extend_from_slice(&self.y1.to_be_bytes());
@@ -105,27 +121,12 @@ impl PgBox {
     }
 }
 
-fn get_f64_from_bytes(bytes: &[u8], start: usize) -> Result<f64, Error> {
-    bytes
-        .get(start..start + BYTE_WIDTH)
-        .ok_or(Error::Decode(
-            format!("Could not decode box bytes: {:?}", bytes).into(),
-        ))?
-        .try_into()
-        .map(f64::from_be_bytes)
-        .map_err(|err| Error::Decode(format!("Invalid bytes slice: {:?}", err).into()))
-}
-
-fn parse_float_from_str(s: &str, error_msg: &str) -> Result<f64, Error> {
-    s.parse().map_err(|_| Error::Decode(error_msg.into()))
-}
-
 #[cfg(test)]
 mod box_tests {
 
     use std::str::FromStr;
 
-    use super::{pg_box_from_bytes, PgBox};
+    use super::PgBox;
 
     const BOX_BYTES: &[u8] = &[
         63, 241, 153, 153, 153, 153, 153, 154, 64, 1, 153, 153, 153, 153, 153, 154, 64, 10, 102,
@@ -134,7 +135,7 @@ mod box_tests {
 
     #[test]
     fn can_deserialise_box_type_bytes() {
-        let pg_box = pg_box_from_bytes(BOX_BYTES).unwrap();
+        let pg_box = PgBox::from_bytes(BOX_BYTES).unwrap();
         assert_eq!(
             pg_box,
             PgBox {
