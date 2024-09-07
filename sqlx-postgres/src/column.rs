b623ee7783ce49462f5398344042116195ab0036
Kurt Wolf <kurtwolfbuilds@gmail.com>
2024-09-06 23:22:28 -0700

    expose relation_id and relation_attribution_no on PgColumn

--- sqlx-postgres/src/column.rs
+++ sqlx-postgres/src/column.rs
@@ -10,9 +10,19 @@ pub struct PgColumn {
     pub(crate) name: UStr,
     pub(crate) type_info: PgTypeInfo,
     #[cfg_attr(feature = "offline", serde(skip))]
-    pub(crate) relation_id: Option<i32>,
+    pub(crate) relation_id: Option<crate::types::Oid>,
     #[cfg_attr(feature = "offline", serde(skip))]
     pub(crate) relation_attribute_no: Option<i16>,
+}
+
+impl PgColumn {
+    pub fn relation_id(&self) -> Option<u32> {
+        self.relation_id.map(|oid| oid.0)
+    }
+
+    pub fn relation_attribute_no(&self) -> Option<i16> {
+        self.relation_attribute_no
+    }
 }
 
 impl Column for PgColumn {
