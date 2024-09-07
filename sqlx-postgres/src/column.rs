18c4be6a8b30b161870660b0e53230354e7d1eee
Kurt Wolf <kurtwolfbuilds@gmail.com>
2024-09-07 12:47:00 -0700

    Update sqlx-postgres/src/column.rs
    Co-authored-by: Austin Bonander <austin.bonander@gmail.com>

--- sqlx-postgres/src/column.rs
+++ sqlx-postgres/src/column.rs
@@ -20,6 +20,12 @@ impl PgColumn {
         self.relation_id.map(|oid| oid.0)
     }
 
+    /// Returns the 1-based index of this column in its parent table, if applicable.
+    ///
+    /// This will be `None` if the column is the result of an expression.
+    ///
+    /// Corresponds to column `attnum` of the `pg_catalog.pg_attribute` table:
+    /// <https://www.postgresql.org/docs/current/catalog-pg-attribute.html>
     pub fn relation_attribute_no(&self) -> Option<i16> {
         self.relation_attribute_no
     }
