0a25254891f878763f481496b2229f6d513eaf73
lilydjwg <lilydjwg@gmail.com>
2024-09-15 14:23:43 +0800

    Add a "sqlite-unbundled" feature that dynamically links to system libsqlite3.so library

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,7 @@ pub use sqlx_mysql::{self as mysql, MySql, MySqlConnection, MySqlExecutor, MySql
 #[doc(inline)]
 pub use sqlx_postgres::{self as postgres, PgConnection, PgExecutor, PgPool, Postgres};
 
-#[cfg(feature = "sqlite")]
+#[cfg(any(feature = "sqlite", feature = "sqlite-unbundled"))]
 #[cfg_attr(docsrs, doc(cfg(feature = "sqlite")))]
 #[doc(inline)]
 pub use sqlx_sqlite::{self as sqlite, Sqlite, SqliteConnection, SqliteExecutor, SqlitePool};
