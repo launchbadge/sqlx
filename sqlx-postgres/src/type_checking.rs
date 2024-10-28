eac4b7aff7826e13b2c87d684dfd37e67b218bf1
Oleksandr Babak <alexanderbabak@proton.me>
2024-10-28 22:59:41 +0100

    Fix: Cannot query Postgres `INTERVAL[]` (#3566)
    * fix(sqlx-postgres): macro could not understand pg interval array type
    
    * refactor(sqlx-postgres): move pg interval array types closer to other array types

--- sqlx-postgres/src/type_checking.rs
+++ sqlx-postgres/src/type_checking.rs
@@ -97,6 +97,7 @@ impl_type_checking!(
         Vec<f64> | &[f64],
         Vec<sqlx::postgres::types::Oid> | &[sqlx::postgres::types::Oid],
         Vec<sqlx::postgres::types::PgMoney> | &[sqlx::postgres::types::PgMoney],
+        Vec<sqlx::postgres::types::PgInterval> | &[sqlx::postgres::types::PgInterval],
 
         #[cfg(feature = "uuid")]
         Vec<sqlx::types::Uuid> | &[sqlx::types::Uuid],
