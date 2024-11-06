ece19befc14760e50b666f5cbb5961b24d86c9bc
James Holman <jameswarringtonholman@gmail.com>
2024-11-06 18:32:24 +1100

    test: array match for geo arrays geo match for geo only

--- sqlx-test/src/lib.rs
+++ sqlx-test/src/lib.rs
@@ -51,11 +51,17 @@ macro_rules! test_type {
         }
     };
 
-    ($name:ident<$ty:ty>($db:ident, $($text:literal @= $value:expr),+ $(,)?)) => {
+    ($name:ident<$ty:ty>($db:ident, $($text:literal ~= $value:expr),+ $(,)?)) => {
         paste::item! {
             $crate::__test_prepared_type!($name<$ty>($db, $crate::[< $db _query_for_test_prepared_geometric_type >]!(), $($text == $value),+));
         }
     };
+    ($name:ident<$ty:ty>($db:ident, $($text:literal = $value:expr),+ $(,)?)) => {
+        paste::item! {
+            $crate::__test_prepared_type!($name<$ty>($db, $crate::[< $db _query_for_test_prepared_geometric_type >]!(), $($text == $value),+));
+        }
+    };
+
 
     ($name:ident($db:ident, $($text:literal == $value:expr),+ $(,)?)) => {
         $crate::test_type!($name<$name>($db, $($text == $value),+));
@@ -233,6 +239,13 @@ macro_rules! Postgres_query_for_test_prepared_type {
 
 #[macro_export]
 macro_rules! Postgres_query_for_test_prepared_geometric_type {
+    () => {
+        "SELECT ({0} ~= $1)::int4, {0}, $2"
+    };
+}
+
+#[macro_export]
+macro_rules! Postgres_query_for_test_prepared_geometric_array_type {
     () => {
         "SELECT ({0} = $1)::int4, {0}, $2"
     };
