d7c49f1219c1cc95412a03ceaf757eb8895d2b8f
Pepe Márquez Romero <pepe.marquezromero@protonmail.com>
2024-11-23 12:40:57 +0100

    fix manifest

--- sqlx-sqlite/src/any.rs
+++ sqlx-sqlite/src/any.rs
@@ -6,7 +6,7 @@ use futures_core::future::BoxFuture;
 use futures_core::stream::BoxStream;
 use futures_util::{StreamExt, TryFutureExt, TryStreamExt};
 
-use sqlx_core::any::{
+pub use sqlx_core::any::{
     Any, AnyArguments, AnyColumn, AnyConnectOptions, AnyConnectionBackend, AnyQueryResult, AnyRow,
     AnyStatement, AnyTypeInfo, AnyTypeInfoKind, AnyValueKind,
 };
