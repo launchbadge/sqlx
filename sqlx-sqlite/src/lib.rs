7ab05a62c7eba5f725a254a427035a31035ffba7
Duncan Fairbanks <duncanfairbanks6@gmail.com>
2024-11-27 18:50:02 -0800

    chore: test begin_with works for all SQLite "BEGIN" statements

--- sqlx-sqlite/src/lib.rs
+++ sqlx-sqlite/src/lib.rs
@@ -46,7 +46,9 @@ use std::sync::atomic::AtomicBool;
 
 pub use arguments::{SqliteArgumentValue, SqliteArguments};
 pub use column::SqliteColumn;
-pub use connection::{LockedSqliteHandle, SqliteConnection, SqliteOperation, UpdateHookResult};
+pub use connection::{
+    LockedSqliteHandle, SqliteConnection, SqliteOperation, SqliteTransactionState, UpdateHookResult,
+};
 pub use database::Sqlite;
 pub use error::SqliteError;
 pub use options::{
