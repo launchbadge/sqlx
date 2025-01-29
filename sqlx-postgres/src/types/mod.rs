97cada3e16a9c875ce4f5af38b0382e72928cb7c
James H. <32926722+jayy-lmao@users.noreply.github.com>
2025-01-30 09:59:50 +1100

    feat(postgres): add geometry box (#3711)
    * feat: add geometry box
    
    * test: cannot compare box arrays
    
    * test: regular equals check for boxes
    
    * test: try box array test

--- sqlx-postgres/src/types/mod.rs
+++ sqlx-postgres/src/types/mod.rs
@@ -24,6 +24,7 @@
 //! | [`PgPoint]                            | POINT                                                |
 //! | [`PgLine]                             | LINE                                                 |
 //! | [`PgLSeg]                             | LSEG                                                 |
+//! | [`PgBox]                              | BOX                                                  |
 //! | [`PgHstore`]                          | HSTORE                                               |
 //!
 //! <sup>1</sup> SQLx generally considers `CITEXT` to be compatible with `String`, `&str`, etc.,
@@ -262,6 +263,7 @@ pub use cube::PgCube;
 pub use geometry::line::PgLine;
 pub use geometry::line_segment::PgLSeg;
 pub use geometry::point::PgPoint;
+pub use geometry::r#box::PgBox;
 pub use hstore::PgHstore;
 pub use interval::PgInterval;
 pub use lquery::PgLQuery;
