1bac964748a08dadb8cc08414c7718828889cf3d
Austin Bonander <austin.bonander@gmail.com>
2025-04-13 22:46:02 -0700

    fix: lots of new Clippy warnings

--- sqlx-sqlite/src/connection/worker.rs
+++ sqlx-sqlite/src/connection/worker.rs
@@ -145,14 +145,14 @@ impl ConnectionWorker {
                     let _guard = span.enter();
                     match cmd {
                         Command::Prepare { query, tx } => {
-                            tx.send(prepare(&mut conn, &query).map(|prepared| {
-                                update_cached_statements_size(
-                                    &conn,
-                                    &shared.cached_statements_size,
-                                );
-                                prepared
-                            }))
-                            .ok();
+                            tx.send(prepare(&mut conn, &query)).ok();
+
+                            // This may issue an unnecessary write on failure,
+                            // but it doesn't matter in the grand scheme of things.
+                            update_cached_statements_size(
+                                &conn,
+                                &shared.cached_statements_size,
+                            );
                         }
                         Command::Describe { query, tx } => {
                             tx.send(describe(&mut conn, &query)).ok();
