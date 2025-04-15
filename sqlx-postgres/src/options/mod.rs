6da7b211ebfb1f674a68d392046c70bd3f0a2466
Kai A. Hiller <git@kaialexhiller.de>
2025-04-15 11:46:44 +0200

    Use an actual config option for the test case

--- sqlx-postgres/src/options/mod.rs
+++ sqlx-postgres/src/options/mod.rs
@@ -721,10 +721,11 @@ fn test_options_formatting() {
         Some("-c geqo=off -c statement_timeout=5min".to_string())
     );
     // https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNECT-OPTIONS
-    let options = PgConnectOptions::new().options([("passfile", r"/back\slash/ and\ spaces")]);
+    let options =
+        PgConnectOptions::new().options([("application_name", r"/back\slash/ and\ spaces")]);
     assert_eq!(
         options.options,
-        Some(r"-c passfile=/back\\slash/\ and\\\ spaces".to_string())
+        Some(r"-c application_name=/back\\slash/\ and\\\ spaces".to_string())
     );
     let options = PgConnectOptions::new();
     assert_eq!(options.options, None);
