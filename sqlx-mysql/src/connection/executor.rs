35b6a79b93c5316935da8c05136cebeb95764099
Austin Bonander <austin.bonander@gmail.com>
2025-06-02 01:18:51 -0700

    fix: run `cargo fmt`

--- sqlx-mysql/src/connection/executor.rs
+++ sqlx-mysql/src/connection/executor.rs
@@ -22,8 +22,8 @@ use futures_core::future::BoxFuture;
 use futures_core::stream::BoxStream;
 use futures_core::Stream;
 use futures_util::TryStreamExt;
-use std::{borrow::Cow, pin::pin, sync::Arc};
 use sqlx_core::column::{ColumnOrigin, TableColumn};
+use std::{borrow::Cow, pin::pin, sync::Arc};
 
 impl MySqlConnection {
     async fn prepare_statement(
