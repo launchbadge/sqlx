1c6ae52bd758f67fae4bf2fb07df760b71c7895c
Joey de Waal <dewaaljoey@gmail.com>
2025-06-06 22:44:24 +0200

    sqlx-postgres: Add io-request for bg worker

---
+++ sqlx-postgres/src/connection/request.rs
@@ -0,0 +1,51 @@
+use futures_channel::mpsc::UnboundedSender;
+use sqlx_core::{io::ProtocolEncode, Error};
+
+use crate::message::{self, EncodeMessage, FrontendMessage, ReceivedMessage};
+
+/// A request for the background worker.
+#[derive(Debug)]
+pub struct IoRequest {
+    pub chan: Option<UnboundedSender<ReceivedMessage>>,
+    pub data: Vec<u8>,
+}
+
+pub struct MessageBuf {
+    data: Vec<u8>,
+}
+
+impl MessageBuf {
+    pub fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+
+    #[inline(always)]
+    pub fn write<'en, T>(&mut self, value: T) -> sqlx_core::Result<()>
+    where
+        T: ProtocolEncode<'en, ()>,
+    {
+        value.encode(&mut self.data)
+    }
+
+    #[inline(always)]
+    pub fn write_sync(&mut self) {
+        self.write_msg(message::Sync)
+            .expect("BUG: Sync should not be too big for protocol");
+    }
+
+    #[inline(always)]
+    pub(crate) fn write_msg(&mut self, message: impl FrontendMessage) -> Result<(), Error> {
+        self.write(EncodeMessage(message))
+    }
+
+    pub(crate) fn buf_mut(&mut self) -> &mut Vec<u8> {
+        &mut self.data
+    }
+
+    pub fn finish(self) -> IoRequest {
+        IoRequest {
+            data: self.data,
+            chan: None,
+        }
+    }
+}
