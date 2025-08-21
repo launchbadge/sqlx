a301d9abad984690f69a4356a5da816d5bdc9319
Xiretza <xiretza@xiretza.xyz>
2025-08-21 20:57:35 +0000

    Allow single-field named structs to be transparent (#3971)
    * Allow single-field named structs to be transparent
    
    This more closely matches the criteria for e.g. #[repr(transparent)]
    and #[serde(transparent)].
    
    * Add tests, fix error messages

--- tests/sqlite/derives.rs
+++ tests/sqlite/derives.rs
@@ -12,3 +12,23 @@ test_type!(origin_enum<Origin>(Sqlite,
     "1" == Origin::Foo,
     "2" == Origin::Bar,
 ));
+
+#[derive(PartialEq, Eq, Debug, sqlx::Type)]
+#[sqlx(transparent)]
+struct TransparentTuple(i64);
+
+#[derive(PartialEq, Eq, Debug, sqlx::Type)]
+#[sqlx(transparent)]
+struct TransparentNamed {
+    field: i64,
+}
+
+test_type!(transparent_tuple<TransparentTuple>(Sqlite,
+    "0" == TransparentTuple(0),
+    "23523" == TransparentTuple(23523)
+));
+
+test_type!(transparent_named<TransparentNamed>(Sqlite,
+    "0" == TransparentNamed { field: 0 },
+    "23523" == TransparentNamed { field: 23523 },
+));
