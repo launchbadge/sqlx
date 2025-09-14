e1ed87557b0fa6ce629af011920c6bb353816c7b
Markus Gasser <markus.gasser@frauscher.com>
2025-09-14 19:41:15 +0200

    Fix migration reverts for no-TX SQLite

--- sqlx-sqlite/src/migrate.rs
+++ sqlx-sqlite/src/migrate.rs
@@ -207,7 +207,7 @@ CREATE TABLE IF NOT EXISTS {table_name} (
             let start = Instant::now();
 
             if migration.no_tx {
-                execute_migration(self, table_name, migration).await?;
+                revert_migration(self, table_name, migration).await?;
             } else {
                 // Use a single transaction for the actual migration script and the essential bookkeeping so we never
                 // execute migrations twice. See https://github.com/launchbadge/sqlx/issues/1966.
