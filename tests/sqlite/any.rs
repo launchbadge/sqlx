410494cd354228645b3c173f2f43ac0f5f7ed580
Michael Guschlbauer <michael.guschlbauer@rohde-schwarz.com>
2026-03-30 12:23:02 +0200

    test(sqlite): add any_sets_last_insert_id
    - added test
    - updated required-features

--- tests/sqlite/any.rs
+++ tests/sqlite/any.rs
@@ -33,3 +33,29 @@ async fn issue_3179() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+/// ensure Any type with SQLite backing returns last_insert_id properly
+/// https://github.com/launchbadge/sqlx/issues/2982
+#[sqlx_macros::test]
+async fn any_sets_last_insert_id() -> anyhow::Result<()> {
+    sqlx::any::install_default_drivers();
+
+    let mut conn = new::<Any>().await?;
+    // syntax as per: https://sqlite.org/autoinc.html
+    let _ = conn
+        .execute(
+            r#"
+CREATE TEMPORARY TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)
+            "#,
+        )
+        .await?;
+
+    let result = sqlx::query("INSERT INTO users (name) VALUES (?)")
+        .bind("Glorbo")
+        .execute(&mut conn)
+        .await?;
+
+    assert_eq!(result.last_insert_id(), Some(1));
+
+    Ok(())
+}
